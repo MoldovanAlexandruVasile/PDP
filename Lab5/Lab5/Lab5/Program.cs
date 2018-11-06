0b5f864507503295ab36a5533c08b56afefbb07f
Moldovan Alenxadru <moldovan_alexandru97@yahoo.com>
2018-11-06 11:08:02 +0200

    Lab 5 done, for real now

--- Lab5/Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Lab5/Program.cs
@@ -16,47 +16,36 @@ namespace WebDownloader
             pages.Add(@"https://stackoverflow.com/questions/42077388/type-used-in-a-using-statement-must-be-implicitly-convertible-to-system-idispos");
             pages.Add(@"https://stackoverflow.com/questions/12931687/targetinvocationexception-occurred-but-i-have-absolutely-no-idea-why");
             pages.Add(@"https://stackoverflow.com/questions/3458046/how-to-include-quotes-in-a-string");
-            Task t = new Task(DownloadPageAsync);
-            t.Start();
+            for (int pos = 0; pos < pages.Count; pos++)
+            {
+                string page = pages.ElementAt(pos);
+                Task.Run(() => DownloadPageEvent(page));
+            }
             Console.ReadLine();
         }
- 
-        static async void DownloadPageAsync()
+
+        static async void DownloadPageAsync(string page)
         {
             HttpClient client = new HttpClient();
-            for (int i = 0; i < pages.Count; i++) {
-                string page = pages.ElementAt(i);
-                HttpResponseMessage response = await client.GetAsync(page);
-                HttpContent content = response.Content;
-                {
-                    string result = await content.ReadAsStringAsync();
-                    Console.WriteLine(result);
-                    separator();
-                }
-            }
+            HttpResponseMessage response = await client.GetAsync(page);
+            HttpContent content = response.Content;
+            string result = await content.ReadAsStringAsync();
+            Console.WriteLine(result);
+            long len = (long)response.Content.Headers.ContentLength;
+            Console.WriteLine("Length of page: " + page +" is " + len);
         }
- 
-        static void DownloadPageEvent()
+
+        static void DownloadPageEvent(string page)
         {
-            for (int i = 0; i < pages.Count; i++)
+            using (WebClient client = new WebClient())
             {
-                using (WebClient client = new WebClient())
+                client.DownloadStringCompleted += (sender, e) =>
                 {
-                    client.DownloadStringCompleted += (sender, e) =>
-                    {
-                        Console.WriteLine(e.Result);
-                    };
-
-                    string page = pages.ElementAt(i);
-                    client.DownloadStringAsync(new Uri(page));
-                    separator();
-                }
+                    Console.WriteLine(e.Result);
+                };
+                client.DownloadStringAsync(new Uri(page));
             }
-        }
 
-        static void separator()
-        {
-            Console.WriteLine("========================================================================================================================");
         }
     }
 }
