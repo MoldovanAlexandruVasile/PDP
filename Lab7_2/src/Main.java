a4540e3faaa12ce8edfb2c6fc47396654874c29d
Moldovan Alenxadru <moldovan_alexandru97@yahoo.com>
2018-11-25 12:33:45 +0200

    Basic implementation for lab7ex2. Have to do something with binary

---
+++ Lab7_2/src/Main.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static List<Integer> number1List = new ArrayList<>();
+    private static List<Integer> number2List = new ArrayList<>();
+    private static List<Integer> number3List = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("\n\n");
+        String number1 = readNumber();
+        String number2 = readNumber();
+        Boolean isFirstLower = false;
+        Integer digitsBigger;
+        if (number1.length() < number2.length()) {
+            isFirstLower = true;
+            digitsBigger = number2.length() - number1.length();
+        } else {
+            digitsBigger = number1.length() - number2.length();
+        }
+
+        if (isFirstLower) {
+            for (Integer i = 0; i < digitsBigger; i++)
+                number1List.add(0);
+            for (Integer i = 0; i < number1.length(); i++)
+                number1List.add(Character.getNumericValue(number1.charAt(i)));
+            for (Integer i = 0; i < number2.length(); i++)
+                number2List.add(Character.getNumericValue(number2.charAt(i)));
+        } else {
+            for (Integer i = 0; i < digitsBigger; i++)
+                number2List.add(0);
+            for (Integer i = 0; i < number2.length(); i++)
+                number2List.add(Character.getNumericValue(number2.charAt(i)));
+            for (Integer i = 0; i < number1.length(); i++)
+                number1List.add(Character.getNumericValue(number1.charAt(i)));
+        }
+
+        for (Integer i = 0; i < number1List.size(); i++)
+            number3List.add(0);
+        Integer remainder = 0;
+        for (Integer i = number1List.size() - 1; i >= 0; i--) {
+            RunnableThread runnableThread = new RunnableThread(number1List, number2List, number3List, remainder, i);
+            runnableThread.start();
+            remainder = runnableThread.getRemainder();
+        }
+
+        String number = "";
+        for (Integer i = 0; i < number3List.size(); i++)
+            number += number3List.get(i);
+        System.out.println("\n\t\tResult: " + number);
+    }
+
+    private static String readNumber() throws IOException {
+        System.out.print("Number: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String number = reader.readLine();
+        return number;
+    }
+}
---
+++ Lab7_2/src/RunnableThread.java
@@ -0,0 +1,51 @@
+import java.util.List;
+
+public class RunnableThread implements Runnable {
+
+    private Thread t;
+    private List<Integer> number1;
+    private List<Integer> number2;
+    private List<Integer> number3;
+    private Integer remainder;
+    private Integer sumPos;
+
+    public RunnableThread(List<Integer> number1, List<Integer> number2, List<Integer> number3, Integer remainder, Integer sumPos) {
+        this.number1 = number1;
+        this.number2 = number2;
+        this.number3 = number3;
+        this.remainder = remainder;
+        this.sumPos = sumPos;
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            Integer first = number1.get(sumPos);
+            Integer second = number2.get(sumPos);
+            Integer sum = first + second + remainder;
+            if (sum >= 10) {
+                sum = sum % 10;
+                setRemainder(1);
+            } else setRemainder(0);
+            number3.set(sumPos, sum);
+        }
+    }
+
+    void start() throws InterruptedException {
+        if (t == null) {
+            t = new Thread(this);
+            synchronized (this) {
+                t.start();
+            }
+            t.join();
+        }
+    }
+
+    public Integer getRemainder() {
+        return remainder;
+    }
+
+    private void setRemainder(Integer remainder) {
+        this.remainder = remainder;
+    }
+}
